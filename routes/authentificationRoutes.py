5e97d1ff8b2346beb5d1793a8d9c3ec859b327ba
aminataCmd <63054248+aminataCmd@users.noreply.github.com>
2024-02-29 21:50:52 +0100

    F1 authentification (#31)
    * new requirements
    
    * add requirements
    
    * modify and add file
    
    * add-cors
    
    * localhost:3000
    
    * modify cors
    
    * modify cors + modify filter by id
    
    * modify version bcrypt, modify services functions
    
    * add fonction
    
    * connexion
    
    * requirements
    
    * auth

--- routes/authentificationRoutes.py
+++ routes/authentificationRoutes.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from services import userServices as users_services
+from services import authentificationServices as auth_services
 from sqlalchemy.orm import Session
 from config.database import SessionLocal, engine
 from models.users import Base
 from schemas.users import *
+from schemas.token import *
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datetime import datetime, timedelta
+from config.settings import settings
 
 Base.metadata.create_all(bind=engine)
+
 
 # Dependency
 def get_db():
@@ -17,6 +23,14 @@ def get_db():
 
 usersRoutes = APIRouter()
 base = "/users"
+
+@usersRoutes.get(f"{base}/usersLogin/")
+def get_all_userslogin(db: Session=Depends(get_db)):
+    return users_services.get_all_usersLogin(db)
+
+@usersRoutes.get(f"{base}/usersLogin"+"/{id}")
+def get_all_userslogin_by_id(id:int, db: Session=Depends(get_db)):
+    return users_services.get_userLogin_by_id(db, id)
 
 @usersRoutes.get(f"{base}/customers/",response_model=list[Customer],status_code=200)
 def get_all_customers(db: Session = Depends(get_db)):
@@ -36,7 +50,8 @@ def get_administrator_by_id(id:int, db: Session = Depends(get_db)):
 
 @usersRoutes.post(f"{base}/customers/")
 def create_customer(customer: CustomerCreate, db: Session = Depends(get_db)):
-    users_services.customer_create(db, customer)
+    return users_services.customer_create(db, customer)
+
 
 @usersRoutes.post(f"{base}/administrators/")
 def create_administrators(administrator: AdministratorCreate, db: Session = Depends(get_db)):
@@ -55,29 +70,22 @@ def update_customer(id: int, customer):
 def delete_customer(id: int, customer: CustomerDelete):
     pass
 
-# @usersRoutes.get(base+'/{id}')
-# async def getById(id):
-#     return await resultVerification(id)
+@usersRoutes.post(f"{base}/token", response_model=Token)
+def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
+    userlogin = auth_services.authenticate_user(db, form_data.username, form_data.password)
+    if not userlogin:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = auth_services.create_access_token(
+        data={"sub": userlogin.id}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
 
-# @usersRoutes.post(base)
-# async def insertUser(userData: UserTemp):
-#     return await service.insertUser(userData)
-
-# @usersRoutes.post(f"{base}/token", response_model=Token)
-# async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-#     user = service.authenticate_user(form_data.username, form_data.password)
-#     if not user:
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail="Incorrect username or password",
-#             headers={"WWW-Authenticate": "Bearer"},
-#         )
-#     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-#     access_token = service.create_access_token(
-#         data={"sub": user["pseudo"]}, expires_delta=access_token_expires
-#     )
-#     return Token(access_token=access_token, token_type="bearer")
-
-# @usersRoutes.get(f"{base}/me/", response_model=User)
-# async def read_users_me(current_user: User = Depends(service.get_current_active_user)):
-#     return current_user
+@usersRoutes.get(f"{base}/me/")
+def read_users_me(db: Session=Depends(get_db), current_user: UserLogin = Depends(auth_services.get_current_user)):
+    return current_user
--- services/utils.py
+++ services/utils.py
@@ -7,12 +7,5 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
-    return pwd_context.hash(password).encode('utf-8')
+    return pwd_context.hash(password)
 
-def authenticate_user(pseudo: str, password: str, db: Session):
-    user = db.usersData.find_one({"pseudo": pseudo})
-    if not user:
-        return False
-    if not verify_password(password, user["hashedPassword"]):
-        return False
-    return user
