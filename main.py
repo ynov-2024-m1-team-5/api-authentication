0b1abef316fbb3c16dc02d8503bb59201ff73313
aminataCmd <63054248+aminataCmd@users.noreply.github.com>
2024-02-29 17:17:19 +0100

    Develop (#21)
    * connection database ok
    
    * models customers and admins ok
    
    * inscription users
    
    * requirements + yml
    
    * new requirements (#4)
    
    * F1 authentification (#7)
    
    * new requirements
    
    * add requirements
    
    * F1 authentification (#11)
    
    * new requirements
    
    * add requirements
    
    * modify and add file
    
    * F1 authentification (#14)
    
    * new requirements
    
    * add requirements
    
    * modify and add file
    
    * add-cors
    
    * F1 authentification (#17)
    
    * new requirements
    
    * add requirements
    
    * modify and add file
    
    * add-cors
    
    * localhost:3000
    
    * F1 authentification (#20)
    
    * new requirements
    
    * add requirements
    
    * modify and add file
    
    * add-cors
    
    * localhost:3000
    
    * modify cors

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    "http://localhost:8080",
-]
+# origins = [
+#     "http://localhost:3000",
+#     "http://localhost:8080",
+# ]
+
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
