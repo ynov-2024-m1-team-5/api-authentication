5b7bb2a0c271dc57d8930fb6a301a750747ed965
aminataCmd <63054248+aminataCmd@users.noreply.github.com>
2024-02-29 16:57:25 +0100

    Release 1.4.1 (#16)
    * Develop (#2)
    
    * connection database ok
    
    * models customers and admins ok
    
    * inscription users
    
    * requirements + yml
    
    * Develop (#5)
    
    * connection database ok
    
    * models customers and admins ok
    
    * inscription users
    
    * requirements + yml
    
    * new requirements (#4)
    
    * Develop (#8)
    
    * connection database ok
    
    * models customers and admins ok
    
    * inscription users
    
    * requirements + yml
    
    * new requirements (#4)
    
    * F1 authentification (#7)
    
    * new requirements
    
    * add requirements
    
    * Release v1.2 (#6)
    
    * Develop (#2)
    
    * connection database ok
    
    * models customers and admins ok
    
    * inscription users
    
    * requirements + yml
    
    * Develop (#5)
    
    * connection database ok
    
    * models customers and admins ok
    
    * inscription users
    
    * requirements + yml
    
    * new requirements (#4)
    
    * r.1.4 (#12)
    
    * connection database ok
    
    * models customers and admins ok
    
    * inscription users
    
    * requirements + yml
    
    * new requirements (#4)
    
    * F1 authentification (#7)
    
    * new requirements
    
    * add requirements
    
    * F1 authentification (#11)
    
    * new requirements
    
    * add requirements
    
    * modify and add file
    
    * r.1.4.1 (#15)
    
    * connection database ok
    
    * models customers and admins ok
    
    * inscription users
    
    * requirements + yml
    
    * new requirements (#4)
    
    * F1 authentification (#7)
    
    * new requirements
    
    * add requirements
    
    * F1 authentification (#11)
    
    * new requirements
    
    * add requirements
    
    * modify and add file
    
    * F1 authentification (#14)
    
    * new requirements
    
    * add requirements
    
    * modify and add file
    
    * add-cors

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from routes.authentificationRoutes import usersRoutes
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app = FastAPI(title="authentification")
 
 app.include_router(usersRoutes, prefix="/api/v1")
