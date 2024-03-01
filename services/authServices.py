48a982a200ab724536e040f422e109d133bc7f3c
aminataCmd <63054248+aminataCmd@users.noreply.github.com>
2024-03-01 14:33:12 +0100

    F1 authentification (#48)
    * new requirements
    
    * add requirements
    
    * modify and add file
    
    * add-cors
    
    * localhost:3000
    
    * modify cors
    
    * modify cors + modify filter by id
    
    * modify version bcrypt, modify services functions
    
    * add fonction
    
    * connexion
    
    * requirements
    
    * auth
    
    * v.1.4.8
    
    * clean
    
    * admin user, creation
    
    * login on page
    
    * log administrator
    
    * send mail
    
    * remove print

--- services/authServices.py
+++ services/authServices.py
@@ -40,7 +40,6 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     return encoded_jwt
 
 def get_current_user(db, token: Annotated[str, Depends(oauth2_scheme)]):
-    print("hehreee")
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
--- services/utils.py
+++ services/utils.py
@@ -1,5 +1,9 @@
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
+from config.settings import settings
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -8,3 +12,19 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+def send_email(subject, content, recipients):
+    msg = MIMEMultipart()
+    msg['From'] = settings.SENDER_EMAIL
+    msg['Subject'] = subject
+    msg.attach(MIMEText(content, 'plain'))
+
+    try:
+        server = smtplib.SMTP(settings.SMTP_SERVER, settings.SMTP_PORT)
+        server.starttls()
+        server.login(settings.SMTP_USERNAME, settings.SMTP_PASSWORD)
+        server.sendmail(settings.SENDER_EMAIL, recipients, msg.as_string())
+        server.quit()
+    except smtplib.SMTPException as e:
+        print("Erreur lors de l'envoi de l'e-mail:", e)
+        raise
